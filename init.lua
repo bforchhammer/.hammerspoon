db83754a3ed1a93587b2f212123d409319e80184
Benedikt Forchhammer <b.forchhammer@gmail.com>
2020-06-22 01:27:04 +0200

    Added spaces menubar widget + caffeine utility

--- init.lua
+++ init.lua
@@ -3,6 +3,7 @@ spoon.SpoonInstall:andUse("ReloadConfiguration", {start=true})
 
 hs.window.animationDuration = 0.1
 
+-- Keyboard shortcuts for window sizing
 local hyper = {"ctrl", "alt", "cmd"}
 spoon.SpoonInstall:andUse("MiroWindowsManager", {hotkeys={
   up = {hyper, "up"},
@@ -12,13 +13,40 @@ spoon.SpoonInstall:andUse("MiroWindowsManager", {hotkeys={
   fullscreen = {hyper, "f"}
 }})
 
+-- Menubar widget to show active spaces; updates on a timer
+local spaceiconsUpdateInterval = 2
 local spaces = require("hs._asm.undocumented.spaces")
+local spaceicons = hs.menubar.new()
+
+function UpdateSpacesIcon()
+    if spaces.isAnimating() then return end
+
+    local activeSpaces = spaces.query(spaces.masks.currentSpaces)
+    local icons = ""
+    for screenId, screenSpaces in pairs(spaces.layout()) do
+	for spaceIdx, spaceId in pairs(screenSpaces) do
+	    if hs.fnutils.contains(activeSpaces, spaceId) then
+		icons = icons .. "🔳"
+            else
+                icons = icons .. "⬜"
+            end
+	end
+	icons = icons .. " "
+    end
+    spaceicons:setTitle(icons)
+end
+local UpdateSpacesIconTimer = hs.timer.doEvery(spaceiconsUpdateInterval, function() UpdateSpacesIcon() end)
+UpdateSpacesIconTimer:fire()
+spaceicons:setClickCallback(UpdateSpacesIcon)
+
+-- Keyboard shortcuts to move windows between screens
 function MoveWindowToSpace(sp)
     local win = hs.window.focusedWindow()      -- current window
     local uuid = win:screen():spacesUUID()     -- uuid for current screen
     local spaceID = spaces.layout()[uuid][sp]  -- internal index for sp
     spaces.moveWindowToSpace(win:id(), spaceID) -- move window to new space
     spaces.changeToSpace(spaceID)              -- follow window to new space
+    UpdateSpacesIconTimer:fire()
 end
 
 local moveSpacesKey = {"ctrl", "shift"}
@@ -32,6 +60,26 @@ hs.hotkey.bind(moveSpacesKey, '7', function() MoveWindowToSpace(7) end)
 hs.hotkey.bind(moveSpacesKey, '8', function() MoveWindowToSpace(8) end)
 hs.hotkey.bind(moveSpacesKey, '9', function() MoveWindowToSpace(9) end)
 
-spoon.SpoonInstall:andUse("HeadphoneAutoPause", {})
+-- Play/pause music players when headphones are connected/disconnected
+spoon.SpoonInstall:andUse("HeadphoneAutoPause", {start=true})
+
+-- Simple utility to keep system alive
+local caffeine = hs.menubar.new()
+function setCaffeineDisplay(state)
+    if state then
+        caffeine:setTitle("☕")
+    else
+        caffeine:setTitle("😴")
+    end
+end
+
+function caffeineClicked()
+    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+end
+if caffeine then
+    caffeine:setClickCallback(caffeineClicked)
+    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+end
+
 
 hs.notify.new({title="Hammerspoon", informativeText="Configuration reloaded"}):send()
