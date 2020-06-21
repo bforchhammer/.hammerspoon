7d8dfe93802c03cbc8eed9b9316b02905b5ca1f1
Benedikt Forchhammer <b.forchhammer@gmail.com>
2020-06-21 17:55:07 +0200

    Added shortcuts to move windows between spaces

--- init.lua
+++ init.lua
@@ -12,7 +12,26 @@ spoon.SpoonInstall:andUse("MiroWindowsManager", {hotkeys={
   fullscreen = {hyper, "f"}
 }})
 
-local spacesKey = {"cmd", "ctrl"}
+local spaces = require("hs._asm.undocumented.spaces")
+function MoveWindowToSpace(sp)
+    local win = hs.window.focusedWindow()      -- current window
+    local uuid = win:screen():spacesUUID()     -- uuid for current screen
+    local spaceID = spaces.layout()[uuid][sp]  -- internal index for sp
+    spaces.moveWindowToSpace(win:id(), spaceID) -- move window to new space
+    spaces.changeToSpace(spaceID)              -- follow window to new space
+end
+
+local moveSpacesKey = {"ctrl", "shift"}
+hs.hotkey.bind(moveSpacesKey, '1', function() MoveWindowToSpace(1) end)
+hs.hotkey.bind(moveSpacesKey, '2', function() MoveWindowToSpace(2) end)
+hs.hotkey.bind(moveSpacesKey, '3', function() MoveWindowToSpace(3) end)
+hs.hotkey.bind(moveSpacesKey, '4', function() MoveWindowToSpace(4) end)
+hs.hotkey.bind(moveSpacesKey, '5', function() MoveWindowToSpace(5) end)
+hs.hotkey.bind(moveSpacesKey, '6', function() MoveWindowToSpace(6) end)
+hs.hotkey.bind(moveSpacesKey, '7', function() MoveWindowToSpace(7) end)
+hs.hotkey.bind(moveSpacesKey, '8', function() MoveWindowToSpace(8) end)
+hs.hotkey.bind(moveSpacesKey, '9', function() MoveWindowToSpace(9) end)
+
 --spoon.SpoonInstall:andUse("MoveSpaces", {hotkeys={
 --  space_left = {spacesKey, "left"},
 --  space_right = {spacesKey, "right"}
