f2d120362b24bcb4a2cd3c32b9a1131c8df26e2c
Benedikt Forchhammer <b.forchhammer@gmail.com>
2022-08-24 08:46:51 +0200

    Updated spaces for monterey

--- init.lua
+++ init.lua
@@ -15,15 +15,12 @@ spoon.SpoonInstall:andUse("MiroWindowsManager", {hotkeys={
 
 -- Menubar widget to show active spaces; updates on a timer
 local spaceiconsUpdateInterval = 1
-local spaces = require("hs._asm.undocumented.spaces")
 local spaceicons = hs.menubar.new()
 
 function UpdateSpacesIcon()
-    if spaces.isAnimating() then return end
-
-    local activeSpaces = spaces.query(spaces.masks.currentSpaces)
+    local activeSpaces = hs.spaces.activeSpaces()
     local icons = ""
-    for screenId, screenSpaces in pairs(spaces.layout()) do
+    for screenId, screenSpaces in pairs(hs.spaces.allSpaces()) do
 	for spaceIdx, spaceId in pairs(screenSpaces) do
 	    if hs.fnutils.contains(activeSpaces, spaceId) then
 		icons = icons .. "🔳"
@@ -39,26 +36,30 @@ local UpdateSpacesIconTimer = hs.timer.doEvery(spaceiconsUpdateInterval, functio
 UpdateSpacesIconTimer:fire()
 spaceicons:setClickCallback(UpdateSpacesIcon)
 
+function GotoSpace(num)
+    hs.eventtap.keyStroke({"ctrl"}, tostring(num), 0)
+end
+
 -- Keyboard shortcuts to move windows between screens
 function MoveWindowToSpace(sp)
-    local win = hs.window.focusedWindow()      -- current window
-    local uuid = win:screen():spacesUUID()     -- uuid for current screen
-    local spaceID = spaces.layout()[uuid][sp]  -- internal index for sp
-    spaces.moveWindowToSpace(win:id(), spaceID) -- move window to new space
-    spaces.changeToSpace(spaceID)              -- follow window to new space
+    local win = hs.window.focusedWindow()
+    local uuid = win:screen():getUUID()
+    local spaceID = hs.spaces.allSpaces()[uuid][sp]
+    hs.spaces.moveWindowToSpace(win, spaceID)
     UpdateSpacesIconTimer:fire()
+    hs.eventtap.keyStroke({"ctrl"}, sp)
 end
 
 local moveSpacesKey = {"ctrl", "shift"}
-hs.hotkey.bind(moveSpacesKey, '1', function() MoveWindowToSpace(1) end)
-hs.hotkey.bind(moveSpacesKey, '2', function() MoveWindowToSpace(2) end)
-hs.hotkey.bind(moveSpacesKey, '3', function() MoveWindowToSpace(3) end)
-hs.hotkey.bind(moveSpacesKey, '4', function() MoveWindowToSpace(4) end)
-hs.hotkey.bind(moveSpacesKey, '5', function() MoveWindowToSpace(5) end)
-hs.hotkey.bind(moveSpacesKey, '6', function() MoveWindowToSpace(6) end)
-hs.hotkey.bind(moveSpacesKey, '7', function() MoveWindowToSpace(7) end)
-hs.hotkey.bind(moveSpacesKey, '8', function() MoveWindowToSpace(8) end)
-hs.hotkey.bind(moveSpacesKey, '9', function() MoveWindowToSpace(9) end)
+hs.hotkey.bind(moveSpacesKey, '1', function() MoveWindowToSpace(1) end, function() GotoSpace(1) end)
+hs.hotkey.bind(moveSpacesKey, '2', function() MoveWindowToSpace(2) end, function() GotoSpace(2) end)
+hs.hotkey.bind(moveSpacesKey, '3', function() MoveWindowToSpace(3) end, function() GotoSpace(3) end)
+hs.hotkey.bind(moveSpacesKey, '4', function() MoveWindowToSpace(4) end, function() GotoSpace(4) end)
+hs.hotkey.bind(moveSpacesKey, '5', function() MoveWindowToSpace(5) end, function() GotoSpace(5) end)
+hs.hotkey.bind(moveSpacesKey, '6', function() MoveWindowToSpace(6) end, function() GotoSpace(6) end)
+hs.hotkey.bind(moveSpacesKey, '7', function() MoveWindowToSpace(7) end, function() GotoSpace(7) end)
+hs.hotkey.bind(moveSpacesKey, '8', function() MoveWindowToSpace(8) end, function() GotoSpace(8) end)
+hs.hotkey.bind(moveSpacesKey, '9', function() MoveWindowToSpace(9) end, function() GotoSpace(9) end)
 
 -- Play/pause music players when headphones are connected/disconnected
 spoon.SpoonInstall:andUse("HeadphoneAutoPause", {start=true})
