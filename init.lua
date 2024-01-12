23b483b9cd40ba164863902072525e71bae731eb
Benedikt Forchhammer <b.forchhammer@gmail.com>
2024-01-13 00:26:36 +0100

    Remove spaces in menubar

--- init.lua
+++ init.lua
@@ -14,48 +14,12 @@ spoon.SpoonInstall:andUse("MiroWindowsManager", {hotkeys={
 }})
 
 
--- Menubar widget to show active spaces
-local spaceicons = hs.menubar.new()
-local spaceSymbols = {'①', '②', '③', '④', '⑤', '⑥', '⑦', '⑧', '⑨'}
-local activeSpaceSymbols = {'➊', '➋', '➌', '➍', '➎', '➏', '➐', '➑', '➒'}
-
-function UpdateSpacesIcon()
-    local activeSpaces = hs.spaces.activeSpaces()
-    local icons = ""
-    local idx = 0
-    for screenId, screenSpaces in pairs(hs.spaces.allSpaces()) do
-        for spaceIdx, spaceId in pairs(screenSpaces) do
-            idx = idx + 1
-            if hs.fnutils.contains(activeSpaces, spaceId) then
-                icons = icons .. activeSpaceSymbols[idx].. " "
-            else
-                icons = icons .. spaceSymbols[idx] .. " "
-            end
-        end
-        icons = icons .. " "
-    end
-    local styledtext = hs.styledtext.new(icons, {font={size=21}, paragraphStyle={lineHeightMultiple=0.8}})
-    spaceicons:setTitle(styledtext)
-end
-
--- Trigger once for initial paint
-UpdateSpacesIcon()
-
--- Trigger when clicked on
-spaceicons:setClickCallback(UpdateSpacesIcon)
-
--- Setup spaces watcher to automatically trigger when active space is changed
-local watcher = hs.spaces.watcher.new(UpdateSpacesIcon)
-watcher:start()
-
-
 -- Keyboard shortcuts to move windows between screens
 function MoveWindowToSpace(sp)
     local win = hs.window.focusedWindow()
     local uuid = win:screen():getUUID()
     local spaceID = hs.spaces.allSpaces()[uuid][sp]
     hs.spaces.moveWindowToSpace(win, spaceID)
-    UpdateSpacesIconTimer:fire()
     hs.eventtap.keyStroke({"ctrl"}, sp)
 end
 
